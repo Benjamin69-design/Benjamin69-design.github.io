db68e37e75a2f26751899c23df3b6083c38256fb
Benjamin69-design <170709550@students.uajy.ac.id>
2019-11-21 10:15:40 +0700

    commit

---
+++ precache-manifest.d10e5bb337f13f649047d47fb72eb8c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771035ec7084ff8ca19a",
+    "url": "/css/chunk-vendors.d198af7f.css"
+  },
+  {
+    "revision": "dab59678e266084be093f38c22c671a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f86d6727a56c9e34a8c1",
+    "url": "/js/app.97936eff.js"
+  },
+  {
+    "revision": "771035ec7084ff8ca19a",
+    "url": "/js/chunk-vendors.ba02b0d7.js"
+  },
+  {
+    "revision": "749c0cc2dbf120e99b14e9cff3ffe767",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
